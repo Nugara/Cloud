27c9c8c450210163e40254c5a917f0f474ff527c
Giuseppe Nugara <nugara.giuseppe@gmail.com>
2022-12-23 05:41:12 +0100

    first commit

---
+++ Cloud/CloudApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloudApp.swift
+//  Cloud
+//
+//  Created by Giuseppe Nugara on 23/12/22.
+//
+
+import SwiftUI
+
+@main
+struct CloudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
